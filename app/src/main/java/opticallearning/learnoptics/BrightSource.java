9b66ab70941051fe40aba6b4173d2602a5c3e0a8
Joel <staujd02@students.ipfw.edu>
2016-06-27 13:45:33 -0400

    Refactored java package to opticallearning, add BrightSource.java, add brightsource.xml, added BrightSource to AndroidManifest.xml, coded the onCreate Method for BrightSource, and added setContent view

---
+++ app/src/main/java/opticallearning/learnoptics/BrightSource.java
@@ -0,0 +1,15 @@
+package opticallearning.learnoptics;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Joel on 6/27/2016.
+ */
+public class BrightSource extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.brightsource);
+    }
+}
--- app/src/main/java/opticallearning/learnoptics/MainActivity.java
+++ app/src/main/java/opticallearning/learnoptics/MainActivity.java
@@ -1,7 +1,6 @@
-package andriodbootcamp.learnoptics;
+package opticallearning.learnoptics;
 
 import android.app.Activity;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends Activity {
