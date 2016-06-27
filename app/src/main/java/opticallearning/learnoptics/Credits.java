d8d3cbf442b137e0c476ba934d35820975033115
Joel <staujd02@students.ipfw.edu>
2016-06-27 14:20:30 -0400

    Added BrightSource, LensCraft, Spectrum Matcher, Pick the Medium, Credits and Portal (extended reading)
    all of the respective modules have their own classes and layout files
    
     - All of the classes are added to the AndroidManifest.xml
     - All of the classes extend Activity
     - All of the classes call setContentView
     - All of the layouts are empty
     - LensCraft has a locked orientation (landscape)

---
+++ app/src/main/java/opticallearning/learnoptics/Credits.java
@@ -0,0 +1,15 @@
+package opticallearning.learnoptics;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Joel on 6/27/2016.
+ */
+public class Credits extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.credits);
+    }
+}
--- app/src/main/java/opticallearning/learnoptics/MainActivity.java
+++ app/src/main/java/opticallearning/learnoptics/MainActivity.java
@@ -2,6 +2,11 @@ package opticallearning.learnoptics;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +14,63 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageButton[] buttons = {
+                (ImageButton) findViewById(R.id.btnMedium),         //0
+                (ImageButton) findViewById(R.id.btnSpectrum),       //1
+                (ImageButton) findViewById(R.id.btnLensCraft),      //2
+                (ImageButton) findViewById(R.id.btnBrightSource),   //3
+                (ImageButton) findViewById(R.id.btnCredits),        //4
+                (ImageButton) findViewById(R.id.btnPortal)          //5
+        };
+
+        buttons[0].setOnClickListener(new View.OnClickListener() {
+            @Override
+            //Pick the Medium
+            public void onClick(View v) {
+
+            }
+        });
+
+        buttons[1].setOnClickListener(new View.OnClickListener() {
+            @Override
+            //Spectrum Matcher
+            public void onClick(View v) {
+
+            }
+        });
+
+        buttons[2].setOnClickListener(new View.OnClickListener() {
+            @Override
+            //LensCraft
+            public void onClick(View v) {
+
+            }
+        });
+
+        buttons[3].setOnClickListener(new View.OnClickListener() {
+            @Override
+            //BrightSource
+            public void onClick(View v) {
+
+            }
+        });
+
+        buttons[4].setOnClickListener(new View.OnClickListener() {
+            @Override
+            //Credits
+            public void onClick(View v) {
+
+            }
+        });
+
+        buttons[5].setOnClickListener(new View.OnClickListener() {
+            @Override
+            //Portal (extended reading)
+            public void onClick(View v) {
+
+            }
+        });
+
     }
 }
