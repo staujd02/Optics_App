31db890ad704e51263b2a1b2f12c4769d9692f9a
Joel <staujd02@students.ipfw.edu>
2016-07-07 11:14:10 -0400

    Committing Drawables
    Changed the lens holders from custom views to image view
    Each lens holder now has a unique name
    Added preliminary constructors to Laser and Lens classes
    Added more fields to Lens and Laser classes
    Added sub classes to Laser -> LaserLine and lens decorator

--- app/src/main/java/opticallearning/learnoptics/Laser.java
+++ app/src/main/java/opticallearning/learnoptics/Laser.java
@@ -1,6 +1,10 @@
 package opticallearning.learnoptics;
 
 import android.graphics.Point;
+import android.graphics.PointF;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 /**
  * Created by Joel on 6/29/2016.
@@ -12,7 +16,35 @@ import android.graphics.Point;
  */
 public class Laser {
 
-    //Point start - this must be defined in constructor;
+    private Point MAX_POINT;
+
+    private PointF start;    //This must be defined in constructor;
+    private PointF End;
+
+    private ArrayList<Lens> lenses;
+    private ArrayList<LaserLens> decoratedLenses;
+
+    private ArrayList<LaserLine> segs;
+
+    public Laser(PointF start,Point maxPoint ){
+        this.start = start;
+        MAX_POINT = maxPoint;
+    }
+
+    public Laser(float x, float y){
+
+    }
+
+    public void addLens(Lens lens){
+        lenses.add(lens);
+    }
+
+    public void addIntialLens(Lens lens, float angle, float distance){
+        LaserLens len = new LaserLens(lens, angle);
+        lenses.add(0,lens);
+    }
+
+
     //Point end - this must be calculated
 
     //Array of line segments defined in points
@@ -29,7 +61,65 @@ public class Laser {
     //getSegments()
     //returns a 2d array of start and end points
     // - for drawing
+    public ArrayList<PointF[]> getSegments(){
+        ArrayList<PointF[]> array = new ArrayList<PointF[]>();
+        LaserLine ll;
+
+        for(int i = 0; i < segs.size(); i++){
+            array.add(segs.get(i).toArray());
+        }
+
+        return array;
+    }
+
+    private void newLaserLine(PointF start, PointF end){
+        LaserLine ll = new LaserLine(start,end);
+        segs.add(ll);
+    }
 
     //Make a y=mx+b and plug in the last simulated x, then you get y
     //end point
+
+    private class LaserLens {
+
+        Lens lens;
+        float entry_Angle;
+        float distance;
+
+        public  LaserLens(Lens lens, float distance){
+            this.lens = lens;
+            this.distance = distance;
+        }
+
+        public  LaserLens(Lens lens,float distance, float entry_Angle){
+            this.lens = lens;
+            this.distance = distance;
+            this.entry_Angle = entry_Angle;
+        }
+
+    }
+
+    private class LaserLine {
+        PointF start;
+        PointF end;
+
+        public LaserLine(PointF start, PointF end){
+            this.start = start;
+            this.end = end;
+        }
+
+        public LaserLine(float x1, float y1, float x2, float y2){
+            start = new PointF(x1,y1);
+            end  = new PointF(x2,y2);
+        }
+
+        public PointF[] toArray(){
+            PointF[] array = new PointF[2];
+            array[0] = start;
+            array[1] = end;
+
+            return array;
+        }
+    }
+
 }
--- app/src/main/java/opticallearning/learnoptics/Lens.java
+++ app/src/main/java/opticallearning/learnoptics/Lens.java
@@ -1,24 +1,76 @@
 package opticallearning.learnoptics;
 
+import android.graphics.Point;
+
 /**
  * Created by Joel on 6/29/2016.
  *
- * Lens object responsible for holding data neccessary for calculations and drawing
+ * Lens object responsible for holding data necessary for calculations and drawing
  * the lens bitmaps
  *
  * This is intended for use in the custom views implemented in Lens Crafter (LensCraft.java)
  */
 public class Lens {
 
-    //ID
-    //Lens shape
-    //Material
-    //Focal Length
-    //index of refraction
-    //Graphic
-    //Description
+    private String id;
+    private String material;
+    private String graphic_Reference;
+    private int fLen;
+    private int nIndex;
+    private boolean concave;
 
-    //gets...
-    //no sets...
+    private Point origin = null;
+    private int height = -1;
+    private int width = -1;
 
+    public Lens(String id, String material, String graphic_Reference, int fLen, int nIndex, boolean concave) {
+        this.id = id;
+        this.material = material;
+        this.graphic_Reference = graphic_Reference;
+        this.fLen = fLen;
+        this.nIndex = nIndex;
+        this.concave = concave;
+    }
+
+    public void setLocation(Point origin, int height, int width){
+        this.origin = origin;
+        this.height = height;
+        this.width = width;
+    }
+
+    public Point getOrigin() {
+        return origin;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getMaterial() {
+        return material;
+    }
+
+    public String getGraphic_Reference() {
+        return graphic_Reference;
+    }
+
+    public int getfLen() {
+        return fLen;
+    }
+
+    public int getnIndex() {
+        return nIndex;
+    }
+
+    public boolean isConcave() {
+        return concave;
+    }
 }
